adf75ef70e1f4b2d9d24050f6747d2c64771c788
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-08-03 12:50:16 +0300

    footer-field

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
+    closeModalBtns: document.querySelectorAll("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtns.forEach((btn) =>
+    btn.addEventListener("click", toggleModal)
+  );
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
